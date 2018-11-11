3c42e0a5df17ce1a115ae3de9df4fccca12df4f0
Angelica Bato <angelica.bato@dowjones.com>
2018-11-11 11:05:44 -0500

    Add playground

---
+++ Magic8-playground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+//This is a string!
+var str = "Hello, playground"
+
+//Create a string that says "Hello, world!"
+
+
+//Create an array that includes several places (You pick the places!)
+
+
+//Use a for-loop that prints out "Hello, __!" for each place in your array
+
+
+
+
+
+//Use Int.random(in:) to print out "Hello, __!" for a random place from your array
+
+
+
+
+
