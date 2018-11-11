d847bc6a46d58d2f69f6ff7231b2a1598e7f8c82
Angelica Bato <angelica.bato@dowjones.com>
2018-11-11 11:15:37 -0500

    Add solutions for playground

--- Magic8-playground.playground/Contents.swift
+++ Magic8-playground.playground/Contents.swift
@@ -4,20 +4,28 @@ import UIKit
 var str = "Hello, playground"
 
 //Create a string that says "Hello, world!"
-
+let world = "Hello, world!"
 
 //Create an array that includes several places (You pick the places!)
+let arrayOfPlaces = ["New York", "Tokyo", "Barcelona", "Los Angeles", "Berlin"]
 
+//Use a for-loop that uses print() to print out "Hello, __!" for each place in your array
 
-//Use a for-loop that prints out "Hello, __!" for each place in your array
+//Option 1
+for place in arrayOfPlaces {
+    print("Hello, " + place + "!")
+}
 
+//Option 2
+for i in 0..<arrayOfPlaces.count {
+    print("Hello, " + arrayOfPlaces[i] + "!")
+}
 
-
-
+//Option 3
+for place in arrayOfPlaces {
+    print("Hello, \(place)!")
+}
 
 //Use Int.random(in:) to print out "Hello, __!" for a random place from your array
-
-
-
-
-
+let randomInt = Int.random(in: 0..<arrayOfPlaces.count)
+print("Hello, \(arrayOfPlaces[randomInt])!")
